13a4fbf4f6d12d6fb79e249cbcb6a148f373914d
karinabertolazzo <mota.de.santana@hotmail.com>
2025-07-10 15:45:05 -0300

    feat: Pacientes

---
+++ Models/Paciente.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsultaPopularApi.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+
+        public string Sexo { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
